5ec7a4230b26848c4b3fdcc432274f1c2c8a13d8
Nuntius Creative Hub <hola@nuntius.dev>
2024-11-23 12:28:55 -0500

    Update de-kde.sh

--- de-kde.sh
+++ de-kde.sh
@@ -3,13 +3,48 @@
 sudo apt update && sudo apt upgrade -y
 
 # Instalar XFCE y LightDM
-sudo apt install xfce4 xfce4-goodies libreoffice zenity git curl wget -y
-sudo apt install lightdm -y
+sudo apt install xfce4 xfce4-goodies zsh libreoffice zenity git curl wget gdebi lightdm chromium-browser firefox feathernotes geany synaptic audacious parole netsurf xarchiver -y
 # Validar que una aplicación se proporciona como argumento
 if [ -z "$1" ]; then
   zenity --error --text="No se especificó ninguna aplicación para ejecutar." --title="Error"
   exit 1
 fi
+# Configuración de Zsh
+cd $HOME
+if [ -f ".zshrc" ]; then
+    mv .zshrc .zshrc.backup
+fi
+
+if [ -f ".zsh_history" ]; then
+    mv .zsh_history .zsh_history.backup
+fi
+
+# Descargar y configurar Zsh
+wget https://github.com/atamshkai/Termux-Zsh/raw/main/zsh.tar.xz
+tar -xvJf zsh.tar.xz
+rm zsh.tar.xz
+
+wget https://github.com/atamshkai/Termux-Desktop-2/raw/main/.zshrc -O ~/.zshrc
+wget https://github.com/atamshkai/Termux-Desktop-2/raw/main/.zsh_history -O ~/.zsh_history
+
+chsh -s $(which zsh)
+
+# Crear scripts para alternar configuraciones (opcional)
+if [ -d "$HOME/.config" ]; then
+    mv ~/.config ~/.config.default
+fi
+
+echo "mv ~/.config ~/.config.windows
+mv ~/.config.default ~/.config
+mv ~/.zshrc ~/.zshrc.windows
+mv ~/.zshrc.default ~/.zshrc" | sudo tee /usr/local/bin/windows2default
+chmod +x /usr/local/bin/windows2default
+
+echo "mv ~/.config ~/.config.default
+mv ~/.config.windows ~/.config
+mv ~/.zshrc ~/.zshrc.default
+mv ~/.zshrc.windows ~/.zshrc" | sudo tee /usr/local/bin/default2windows
+chmod +x /usr/local/bin/default2windows
 
 # Ejecutar la aplicación con la variable DISPLAY configurada
 env DISPLAY=:0 "$@" &> /dev/null &
@@ -86,7 +121,7 @@ echo "startxfce4 &" >> ~/.bashrc
 # Limpiar archivos innecesarios
 sudo apt autoremove -y
 sudo apt clean
-
+rm -rf $HOME/zsh.tar.xz
 # Mensaje final
 clear
 echo ""
